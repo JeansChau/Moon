191df02cc98b925f5146376f5f2b56c85f30f685
Zzz <82641682+JeansChau@users.noreply.github.com>
2024-01-02 11:34:16 +0800

    Test luisa compute integration.

--- Source/Test/main.cpp
+++ Source/Test/main.cpp
@@ -1,11 +1,28 @@
-#include <stdio.h>
-#include<iostream>
 #include <iostream>
-#include <list>
-#include <algorithm>
+#include <luisa/luisa-compute.h>
 
-int main()
+using namespace luisa;
+using namespace luisa::compute;
+using luisa::compute::detail::FunctionBuilder;
+int main(int argc, char* argv[])
 {
+	luisa::log_level_verbose();
+	luisa::compute::Context ctx{argv[0]};
+	auto device = ctx.create_device("cuda");
+	Stream stream = device.create_stream();
+	Buffer<int> buf = device.create_buffer<int>(100);
+	auto h = 1.0_h;
+	auto f = sin(h);
+	LUISA_INFO("h = {}, f = {}, f * f = {}, f + h = {}", h, f, f * f, f + h);
+
+	Kernel1D k1 = [&] {
+		buf->write(1, 43);
+		};
+
+	Shader1D<> s = device.compile(k1);
+	stream << s().dispatch(1024u);
+	stream << synchronize();
+
 	std::cout << "This is the \"Moon\" framework" << std::endl;
 	return 0;
 }
